23a6c1f38a37667d0ddf264639ef16faf2c28071
Lucas Vieira <lucasvieiramoura@gmail.com>
2021-02-01 09:41:42 -0300

    Enviar para Logisitca somente produto que possui contrato ou cotação junto com serviço

--- events/laf_log_001.sql
+++ events/laf_log_001.sql
@@ -1,21 +1,32 @@
 -- LU: CustomerOrder
 -- Table: CUSTOMER_ORDER_TAB
 -- CAMPOS: ORDER_NO, ORDER_ID, ROWSTATE
--- Condição de execução ROWSTATE = Released
+-- Condição de execução ORDER_ID != SEO
 
 DECLARE 
 
+
 attr_  VARCHAR2(3200);
 order_id_ VARCHAR2(3200) := '&NEW:ORDER_NO';
+quotation_no_ VARCHAR2(3200) :='&NEW:QUOTATION_NO';
+agreement_id_  VARCHAR2(3200) := '&NEW:C_AGREEMENT_ID';
 
 BEGIN
 
-IF '&NEW:ORDER_ID' = 'OV' OR '&NEW:ORDER_ID' ='OR' OR '&NEW:ORDER_ID' ='SCT' THEN
+IF '&NEW:ORDER_ID' = 'OV' OR '&NEW:ORDER_ID' = 'OR' OR '&NEW:ORDER_ID' ='SBT' THEN
 
   client_sys.Add_To_Attr('orderId_',order_id_, attr_);
+  client_sys.Add_To_Attr('agreementId_',agreement_id_, attr_);
+  client_sys.Add_To_Attr('quotationId_',quotation_no_, attr_);
 
   transaction_SYS.Deferred_Call('LAF_LOG_001', attr_,'Create Logistics Order');
 
 END IF;
 
+EXCEPTION WHEN NO_DATA_FOUND THEN 
+BEGIN
+
+dbms_output.put_line('Dados não encontrados');
+
+END;
 END;
--- procedures background/LAF_LOG_001.sql
+++ procedures background/LAF_LOG_001.sql
@@ -17,37 +17,61 @@ create or replace procedure laf_log_001(attr_ in varchar2) is
   -- p4 -> __sAction
   p4_ VARCHAR2(32000) := 'DO';
 
-  --EXISTS_LOGISTICS_ VARCHAR2(3200) := NULL;
-  ORDER_NO_         VARCHAR2(3200) := client_sys.Get_Item_Value('orderId_',attr_);
-  LINE_NO_          VARCHAR2(3200) := '';
-  REL_NO_           VARCHAR2(3200) := '';
-  --ORDER_ID_         VARCHAR2(3200) := NULL;
+  EXISTS_SERVICE_ VARCHAR2(3200) := NULL;
+  ORDER_NO_       VARCHAR2(3200) := client_sys.Get_Item_Value('orderId_',
+                                                              attr_);
+  LINE_NO_        VARCHAR2(3200) := '';
+  REL_NO_         VARCHAR2(3200) := '';
+  QUOTATION_ID_   VARCHAR2(3200) := client_sys.Get_Item_Value('quotationId_',
+                                                              attr_);
+  AGREEMENT_ID_   VARCHAR2(3200) := client_sys.Get_Item_Value('agreementId_',
+                                                              attr_);
 
   CURSOR GET_SALES_ORDER IS
     SELECT ORDER_NO, LINE_NO, REL_NO
       FROM CUSTOMER_ORDER_LINE
      WHERE ORDER_NO = ORDER_NO_;
 
+  CURSOR GET_EXISTS_SERVICES IS
+    select *
+      from dual
+     where EXISTS (select *
+              from c_rec_agreement_item_serv ais
+             inner join c_rec_agreement_item ai
+                on ais.agreement_id = ai.agreement_id
+             where ai.agreement_id = AGREEMENT_ID_
+               and ai.delivery_order_no = ORDER_NO_)
+        OR EXISTS (select *
+              from order_quotation_line
+             where catalog_type = 'Mat Não Estocável'
+               and quotation_no = QUOTATION_ID_);
+
 BEGIN
+
+  OPEN GET_EXISTS_SERVICES;
+  FETCH GET_EXISTS_SERVICES
+    INTO EXISTS_SERVICE_;
+  CLOSE GET_EXISTS_SERVICES;
+
+  IF EXISTS_SERVICE_ IS NOT NULL THEN
   
-
-        OPEN GET_SALES_ORDER;
-        LOOP
-          FETCH GET_SALES_ORDER
-            INTO ORDER_NO_, LINE_NO_, REL_NO_;
-
-          p3_ := 'CF$_ORDER_NO' || chr(31) || ORDER_NO_ || chr(30) ||
-                 'CF$_TYPE_LOGISTICS' || chr(31) || 'MOBILIZACAO' ||
-                 chr(30) || 'CF$_ORDER_LINE_NO' || chr(31) || LINE_NO_ ||
-                 chr(30) || 'CF$_ORDER_REL_NO' || chr(31) || REL_NO_ ||
-                 chr(30) || 'CF$_STATUS_LOGISTICS_DB' || chr(31) || 'NO' ||
-                 chr(30);
-
-          EXIT WHEN GET_SALES_ORDER%NOTFOUND;
-          IFSLAF.LAF_LOGISTICS_SOLUTION_CLP.NEW__(p0_, p1_, p2_, p3_, p4_);
-
-        END LOOP;
-        CLOSE GET_SALES_ORDER;
-        
-        COMMIT;
+    OPEN GET_SALES_ORDER;
+    LOOP
+      FETCH GET_SALES_ORDER
+        INTO ORDER_NO_, LINE_NO_, REL_NO_;
+    
+      p3_ := 'CF$_ORDER_NO' || chr(31) || ORDER_NO_ || chr(30) ||
+             'CF$_TYPE_LOGISTICS' || chr(31) || 'MOBILIZACAO' || chr(30) ||
+             'CF$_ORDER_LINE_NO' || chr(31) || LINE_NO_ || chr(30) ||
+             'CF$_ORDER_REL_NO' || chr(31) || REL_NO_ || chr(30) ||
+             'CF$_STATUS_LOGISTICS_DB' || chr(31) || 'NO' || chr(30);
+    
+      EXIT WHEN GET_SALES_ORDER%NOTFOUND;
+      IFSLAF.LAF_LOGISTICS_SOLUTION_CLP.NEW__(p0_, p1_, p2_, p3_, p4_);
+    
+    END LOOP;
+    CLOSE GET_SALES_ORDER;
+  
+    COMMIT;
+  END IF;
 END;
