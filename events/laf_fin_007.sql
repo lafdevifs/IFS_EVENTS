f43916b0e3413eb98dec23a320d60dc206dcd00b
Lucas Moura <lucas.moura@lafaete.corp>
2021-04-15 21:39:59 -0300

    Inclusão do campo data de requisição da análise de crédito nos evneto laf_fin_007 e laf_fin_008

--- events/laf_fin_007.sql
+++ events/laf_fin_007.sql
@@ -14,12 +14,22 @@ DECLARE
    p2_ VARCHAR2(32000) := NULL;
 
    -- p3 -> __lsAttr
-   p3_ VARCHAR2(32000) := 'CF$_QUOTATION_NO'||chr(31)|| '&NEW:QUOTATION_NO'||chr(30);
+   p3_ VARCHAR2(32000) := NULL;
 
    -- p4 -> __sAction
    p4_ VARCHAR2(32000) := 'DO';
 
+   REQUEST_DATE_  VARCHAR(3200);
+
 BEGIN
+
+select TO_CHAR(SYSDATE,'YYYY-MM-DD HH24:MI:SS')
+INTO REQUEST_DATE_
+from dual;
+
+
+p3_ :=  'CF$_QUOTATION_NO'||chr(31)|| '&NEW:QUOTATION_NO'||chr(30)  ||  'CF$_APPROVED_ANALYSIS_CRED_DB' || chr(31) || '2' || chr(30)  ||  'CF$_REQUEST_DATE' || chr(31) ||  REQUEST_DATE_ || chr(30);
+
     
 IFSLAF.LAF_CREDIT_ANALYSIS_CLP.NEW__( p0_ , p1_ , p2_ , p3_ , p4_ );
 
--- events/laf_fin_008.sql
+++ events/laf_fin_008.sql
@@ -4,25 +4,38 @@
 -- Acionamentos: ROWSTATE = Released
 
 DECLARE
-   -- p0 -> __lsResult
-   p0_ VARCHAR2(32000) := NULL;
+  -- p0 -> __lsResult
+  p0_ VARCHAR2(32000) := NULL;
 
-   -- p1 -> __sObjid
-   p1_ VARCHAR2(32000) := NULL;
+  -- p1 -> __sObjid
+  p1_ VARCHAR2(32000) := NULL;
 
-   -- p2 -> __lsObjversion
-   p2_ VARCHAR2(32000) := NULL;
+  -- p2 -> __lsObjversion
+  p2_ VARCHAR2(32000) := NULL;
 
-   -- p3 -> __lsAttr
-   p3_ VARCHAR2(32000) := 'CF$_AGREEMENT_ID'||chr(31)|| '&NEW:AGREEMENT_ID'||chr(30) ||  'CF$_AGREEMENT_LINE_NO'||chr(31)|| '&NEW:LINE_NO'||chr(30);
+  -- p3 -> __lsAttr
+  p3_ VARCHAR2(32000) := NULL;
 
-   -- p4 -> __sAction
-   p4_ VARCHAR2(32000) := 'DO';
 
-STATUS_AGREEMENT_ VARCHAR(3200);
+  -- p4 -> __sAction
+  p4_ VARCHAR2(32000) := 'DO';
+
+  REQUEST_DATE_  VARCHAR(3200);
 
 BEGIN
+select TO_CHAR(SYSDATE,'YYYY-MM-DD HH24:MI:SS')
+INTO REQUEST_DATE_
+from dual;
+
+p3_  := 'CF$_AGREEMENT_ID' || chr(31) ||
+                         '&NEW:AGREEMENT_ID' || chr(30) ||
+                         'CF$_AGREEMENT_LINE_NO' || chr(31) ||
+                         '&NEW:LINE_NO' || chr(30) ||  'CF$_APPROVED_ANALYSIS_CRED_DB' || chr(31) ||
+                         '2' || chr(30)  ||  'CF$_REQUEST_DATE' || chr(31) ||  REQUEST_DATE_ || chr(30);
+
     
-IFSLAF.LAF_CREDIT_ANALYSIS_CLP.NEW__( p0_ , p1_ , p2_ , p3_ , p4_ );
+
+      IFSLAF.LAF_CREDIT_ANALYSIS_CLP.NEW__(p0_, p1_, p2_, p3_, p4_);
+
 
 END;
