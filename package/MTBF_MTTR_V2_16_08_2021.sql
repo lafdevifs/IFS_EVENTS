024db9a681006efd2e31a106ed133dd1e02ad2c3
lucasvieiramoura <lucasvieiramoura@gmail.com>
2021-08-24 19:01:56 -0300

    View MTBF & MTTR Version 3

--- package/MTBF_MTTR_V2_16_08_2021.sql
+++ package/MTBF_MTTR_V2_16_08_2021.sql
@@ -154,45 +154,44 @@ SELECT LRM.CONTRACT, LRM.MCH_CODE, LRM.MCH_NAME, LRM.MES_ANO, SUM(LRM.RODAGEM) R
         )
     ) MTTR
 FROM (
-SELECT EOM.CONTRACT, EOM.MCH_CODE, EOM.MCH_NAME, EOM.MES_ANO , EOM.RODAGEM 
-FROM (  SELECT EOM.CONTRACT, EOM.MCH_CODE, IFSLAF.Equipment_Object_API.Get_Mch_Name(EOM.CONTRACT, EOM.MCH_CODE) AS  MCH_NAME 
-               ,CASE 
-                 WHEN (SELECT NVL(SUM(COUNT(DISTINCT(WO_NO))),0) AS OS  FROM  IFSLAF.JT_TASK_UIV JTU -- VERIFICA SE POSSIU O.S. PARA O HORIMETRO
-                                                      WHERE JTU.work_type_id = 'COR'
-                                                      AND JTU.ACTUAL_OBJECT_ID = EOM.MCH_CODE
-                                                      AND JTU.actual_object_site = EOM.CONTRACT
-                                                      AND JTU.OBJSTATE IN ('FINISHED','WORKDONE')
-                                                      AND SUBSTR(TRUNC(JTU.actual_finish),4,6) = substr(TRUNC(EOM.REG_DATE),4,6)
-                                                      GROUP BY SUBSTR(TRUNC(JTU.actual_finish),4,6) ) = 0 
-                    AND ( SELECT NVL(SUM(COUNT(DISTINCT(WO_NO))),0) AS OS  FROM  IFSLAF.JT_TASK_UIV JTU  -- VERIFICA SE POSSUI MAIS DE UMA O.S. PARA O PROXIMO MES DO HORIMETRO
-                                                      WHERE JTU.work_type_id = 'COR'
-                                                      AND JTU.ACTUAL_OBJECT_ID = EOM.MCH_CODE
-                                                      AND JTU.actual_object_site = EOM.CONTRACT
-                                                      AND JTU.OBJSTATE IN ('FINISHED','WORKDONE')
-                                                      AND NOT EXISTS (SELECT SUBSTR(TO_CHAR(ADD_MONTHS((TRUNC(EOM.REG_DATE)),1),'DD/MM/YY'),4,6) FROM DUAL)  
-                                                      GROUP BY SUBSTR(TRUNC(JTU.actual_finish),4,6)   ) > 1
-                   AND ( substr(TRUNC(EOM.REG_DATE),4,6)) != (SUBSTR(TRUNC(SYSDATE),4,6))                       -- VERIFICA SE O MES VERIFICADO DO HORIMETRO NAO E O ATUA, 
-                 THEN SUBSTR(TO_CHAR(ADD_MONTHS((TRUNC(EOM.REG_DATE)),1),'DD/MM/YY'),4,6)  -- ADICINA 1 MES NO MES DO HORIMETOS LANÇADO, SE O HORIMETOR NÃO POSSUI O.S., O PROXIMO MES O POSSUI PELO MENOS UMA O.S. E O MES DO HORIMETRO NÃO É MES ATUAL.
-             ELSE SUBSTR(TRUNC(EOM.REG_DATE),4,6) 
-             END MES_ANO
-              ,SUM((EOM.MEASURED_VALUE - IFSLAF.Laf_Equipment_Utils_Api.Laf_Get_Prev_Recorded_Value_By_Date(EOM.reg_date,EOM.contract,EOM.mch_code,EOM.test_point_id, EOM.parameter_code)) ) AS RODAGEM 
-                       FROM   IFSLAF.EQUIPMENT_OBJECT_MEAS_TAB EOM
-                       WHERE EOM.parameter_code = 'HR'
-                       AND EOM.test_point_id = '*'
-                       AND EOM.Measurement_Type = 'RecordedReading'
-                       AND EOM.reg_date > TO_DATE('01/04/2021','DD/MM/YYYY')
-                       GROUP BY 
-                       EOM.CONTRACT, EOM.MCH_CODE, IFSLAF.Equipment_Object_API.Get_Mch_Name(EOM.CONTRACT, EOM.MCH_CODE) 
-                       , substr(TRUNC(EOM.REG_DATE),4,6) 
-                       ,SUBSTR(TO_CHAR(ADD_MONTHS((TRUNC(EOM.REG_DATE)),1),'DD/MM/YY'),4,6)
-      ) EOM 
-LEFT JOIN IFSLAF.JT_TASK_UIV JTUE
-              ON ( JTUE.actual_object_site =  EOM.CONTRACT 
-              AND JTUE.actual_object_id = EOM.mch_code 
-              AND JTUE.OBJSTATE IN ('FINISHED','WORKDONE') )
-WHERE EOM.MES_ANO = SUBSTR(TRUNC(JTUE.actual_finish),4,6)
-GROUP BY EOM.CONTRACT, EOM.CONTRACT, EOM.MCH_CODE, EOM.MCH_NAME ,  EOM.MES_ANO ,EOM.RODAGEM
-)LRM
-WHERE LRM.contract = '10'
+    SELECT EOM.CONTRACT, EOM.MCH_CODE, EOM.MCH_NAME, EOM.MES_ANO , EOM.RODAGEM 
+        FROM (  SELECT EOM.CONTRACT, EOM.MCH_CODE, IFSLAF.Equipment_Object_API.Get_Mch_Name(EOM.CONTRACT, EOM.MCH_CODE) AS  MCH_NAME 
+                    ,CASE 
+                        WHEN (SELECT NVL(SUM(COUNT(DISTINCT(WO_NO))),0) AS OS  FROM  IFSLAF.JT_TASK_UIV JTU -- VERIFICA SE POSSIU O.S. PARA O HORIMETRO
+                                                            WHERE JTU.work_type_id = 'COR'
+                                                            AND JTU.ACTUAL_OBJECT_ID = EOM.MCH_CODE
+                                                            AND JTU.actual_object_site = EOM.CONTRACT
+                                                            AND JTU.OBJSTATE IN ('FINISHED','WORKDONE')
+                                                            AND SUBSTR(TRUNC(JTU.actual_finish),4,6) = substr(TRUNC(EOM.REG_DATE),4,6)
+                                                            GROUP BY SUBSTR(TRUNC(JTU.actual_finish),4,6) ) = 0 
+                            AND ( SELECT NVL(SUM(COUNT(DISTINCT(WO_NO))),0) AS OS  FROM  IFSLAF.JT_TASK_UIV JTU  -- VERIFICA SE POSSUI MAIS DE UMA O.S. PARA O PROXIMO MES DO HORIMETRO
+                                                            WHERE JTU.work_type_id = 'COR'
+                                                            AND JTU.ACTUAL_OBJECT_ID = EOM.MCH_CODE
+                                                            AND JTU.actual_object_site = EOM.CONTRACT
+                                                            AND JTU.OBJSTATE IN ('FINISHED','WORKDONE')
+                                                            AND NOT EXISTS (SELECT SUBSTR(TO_CHAR(ADD_MONTHS((TRUNC(EOM.REG_DATE)),1),'DD/MM/YY'),4,6) FROM DUAL)  
+                                                            GROUP BY SUBSTR(TRUNC(JTU.actual_finish),4,6)   ) > 1
+                        AND ( substr(TRUNC(EOM.REG_DATE),4,6)) != (SUBSTR(TRUNC(SYSDATE),4,6))                       -- VERIFICA SE O MES VERIFICADO DO HORIMETRO NAO E O ATUA, 
+                        THEN SUBSTR(TO_CHAR(ADD_MONTHS((TRUNC(EOM.REG_DATE)),1),'DD/MM/YY'),4,6)  -- ADICINA 1 MES NO MES DO HORIMETOS LANÇADO, SE O HORIMETOR NÃO POSSUI O.S., O PROXIMO MES O POSSUI PELO MENOS UMA O.S. E O MES DO HORIMETRO NÃO É MES ATUAL.
+                    ELSE SUBSTR(TRUNC(EOM.REG_DATE),4,6) 
+                    END MES_ANO
+                    ,SUM((EOM.MEASURED_VALUE - IFSLAF.Laf_Equipment_Utils_Api.Laf_Get_Prev_Recorded_Value_By_Date(EOM.reg_date,EOM.contract,EOM.mch_code,EOM.test_point_id, EOM.parameter_code)) ) AS RODAGEM 
+                            FROM   IFSLAF.EQUIPMENT_OBJECT_MEAS_TAB EOM
+                            WHERE EOM.parameter_code = 'HR'
+                            AND EOM.test_point_id = '*'
+                            AND EOM.Measurement_Type = 'RecordedReading'
+                            AND EOM.reg_date > TO_DATE('01/04/2021','DD/MM/YYYY')
+                            GROUP BY 
+                            EOM.CONTRACT, EOM.MCH_CODE, IFSLAF.Equipment_Object_API.Get_Mch_Name(EOM.CONTRACT, EOM.MCH_CODE) 
+                            , substr(TRUNC(EOM.REG_DATE),4,6) 
+                            ,SUBSTR(TO_CHAR(ADD_MONTHS((TRUNC(EOM.REG_DATE)),1),'DD/MM/YY'),4,6)
+    ) EOM 
+    LEFT JOIN IFSLAF.JT_TASK_UIV JTUE
+                ON ( JTUE.actual_object_site =  EOM.CONTRACT 
+                AND JTUE.actual_object_id = EOM.mch_code 
+                AND JTUE.OBJSTATE IN ('FINISHED','WORKDONE') )
+    WHERE EOM.MES_ANO = SUBSTR(TRUNC(JTUE.actual_finish),4,6)
+    GROUP BY EOM.CONTRACT, EOM.CONTRACT, EOM.MCH_CODE, EOM.MCH_NAME ,  EOM.MES_ANO ,EOM.RODAGEM
+) LRM
 GROUP BY LRM.CONTRACT, LRM.MCH_CODE, LRM.MCH_NAME, LRM.MES_ANO;
 
