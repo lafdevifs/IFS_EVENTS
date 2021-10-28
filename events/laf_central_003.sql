c2531f3e62886396e70f7ac3e24fd3c75a43e197
lucasvieiramoura <lucasvieiramoura@gmail.com>
2021-10-28 15:04:30 -0300

    LAF_CENTRAL_003 Loop condição de recebimento da NF comparadas com condição de todas a O.C. relacionadas

--- events/laf_central_003.sql
+++ events/laf_central_003.sql
@@ -2,16 +2,35 @@
 -- LU : FiscalNoteTemp
 -- Table: FISCAL_NOTE_TEMP_TAB
 -- Ação: Novos
--- Campos : ADDRESSEE_ID, INPUT_OUTPUT_TYPE, PAY_TERM_ID, CONTRACT
--- Campo Personalizado: PAY_TER_OC : PURCHASE_ORDER_API.Get_Pay_Term_Id(PURCHASE_ORDER_INFO(&NEW:ADDRESSEE_ID,&NEW:CONTRACT)) 
+-- Campos : INPUT_OUTPUT_TYPE,  FISCAL_NOTE_TYPE
+DECLARE
+  PRAGMA AUTONOMOUS_TRANSACTION;
 
+  PAY_TERM_PO_ VARCHAR2(50);
+  INPUT_PAY_TERM_ VARCHAR2(50) := '&NEW:PAY_TERM_ID';
+
+  CURSOR GET_PAY_TERM_PO IS
+    SELECT PURCHASE_ORDER_API.Get_Pay_Term_Id(ORDER_NO)
+      FROM FN_CUST_PURC_ORDER
+     WHERE FISCAL_NOTE_ID = &NEW:FISCAL_NOTE_ID;
 
 BEGIN
 
-if (&NEW:PAY_TERM_ID != &PAY_TER_OC)  THEN
+  OPEN GET_PAY_TERM_PO;
+  LOOP
+    FETCH GET_PAY_TERM_PO
+      INTO PAY_TERM_PO_;
 
-raise_application_error(-20100 ,'Laf_Central_NF_003 Diz: Condição de Pagamento difere da proposta na ordem de compra');
-
-end if;
+  IF PAY_TERM_PO_ <> INPUT_PAY_TERM_  THEN
+    
+      raise_application_error(-20100,
+                              'Laf_Central_NF_003 Diz: Condição de Pagamento inserida ' || INPUT_PAY_TERM_ || ' é difere da proposta na ordem de compra '||PAY_TERM_PO_ || ' !' );
+    
+    END IF;
+  
+    EXIT WHEN GET_PAY_TERM_PO%NOTFOUND;
+  
+  END LOOP;
+  CLOSE GET_PAY_TERM_PO;
 
 END;
